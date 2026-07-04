6377a39aec66e6747638df3d64f1455f6285c760
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-04 16:33:35 +0100

    get.api for nasa api using axios

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.use(express.json());
 
 app.get('/api', (req, res) => {res.send('Asteroid Radar Backend API is running')});
 
+ app.get('/api/asteroids', async (req, res) => {
+    try {
+        const apiKey = process.env.NASA_API_KEY;
+        const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${apiKey}`);
+        res.json(response.data);
+    }catch (error) {
+        console.error('Error fetching asteroid data:', error);
+        res.status(500).json({ error: 'Failed to fetch asteroid data' }); 
+    }
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
