54d4b54173f3fadb8f1f733f2010dc497fc151b7
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-06 19:59:49 +0100

    updated app.js: added enpoint for APOD and Geomagnetic storm

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// imports ------------------------------
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv/config'
@@ -6,8 +7,11 @@ import axios from 'axios';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware -----------------------------
 app.use(cors());
 app.use(express.json());
+
+// Endpoints ------------------------------
 
 app.get('/api', (req, res) => {res.send('Asteroid Radar Backend API is running')});
 
@@ -21,6 +25,31 @@ app.get('/api', (req, res) => {res.send('Asteroid Radar Backend API is running')
         res.status(500).json({ error: 'Failed to fetch asteroid data' }); 
     }
 });
+
+app.get('/api/apod', async (req, res) => {
+    try{
+        const apiKey = process.env.NASA_API_KEY;
+        const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+        res.json(response.data);
+    }catch(error){
+        console.error('Error fetching APOD data:', error);
+        res.status(500).json({ error: 'Failed to fetch APOD data' });
+    }
+});
+
+app.get('/api/geomagnetic', async (req, res) => {
+    try{
+        const apiKey = process.env.NASA_API_KEY;
+        const response = await axios.get(`https://api.nasa.gov/DONKI/GST?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key=${apiKey}`);
+        res.json(response.data);
+    }catch(error){
+        console.error('Error fetching geomagnetic storm data:', error);
+        res.status(500).json({ error: 'Failed to fetch geomagnetic storm data' });
+    }
+});
+
+// Server starts ------------------------------
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
