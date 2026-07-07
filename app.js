5a6599f871e08422f519e37a4ddd1fefdd16e49f
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-07 17:50:15 +0100

    updated app.js for rendering to frontend

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware -----------------------------
-app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // Endpoints ------------------------------
 
