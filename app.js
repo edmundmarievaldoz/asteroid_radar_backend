db3b99fc9e1312d6b40eb08d25b4132724745c11
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-07 19:35:44 +0100

    refactor

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const apiKey = process.env.NASA_API_KEY;
 
 // middleware -----------------------------
 app.use(express.json());
@@ -21,7 +22,6 @@ app.get('/api', (req, res) => {res.send('Asteroid Radar Backend API is running')
 
  app.get('/api/asteroids', async (req, res) => {
     try {
-        const apiKey = process.env.NASA_API_KEY;
         const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${apiKey}`);
         res.json(response.data);
     }catch (error) {
@@ -32,7 +32,6 @@ app.get('/api', (req, res) => {res.send('Asteroid Radar Backend API is running')
 
 app.get('/api/apod', async (req, res) => {
     try{
-        const apiKey = process.env.NASA_API_KEY;
         const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
         res.json(response.data);
     }catch(error){
@@ -43,7 +42,6 @@ app.get('/api/apod', async (req, res) => {
 
 app.get('/api/geomagnetic', async (req, res) => {
     try{
-        const apiKey = process.env.NASA_API_KEY;
         const response = await axios.get(`https://api.nasa.gov/DONKI/GST?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key=${apiKey}`);
         res.json(response.data);
     }catch(error){
